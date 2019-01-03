5ed74287007020bcba1584130c71b0531edb6639
v.kanaev <v.kanaev@potok.digital>
2019-01-03 13:02:52 +0300

    Фибоначи

---
+++ 09-sum-to/fib.js
@@ -0,0 +1,21 @@
+/*
+  function fib(n)
+
+  alert( fib(3) ); // 2
+  alert( fib(7) ); // 13
+  alert( fib(77)); // 5527939700884757
+*/
+
+function fib(n) {
+  let x = 1;
+  let y = 0;
+  let i;
+
+  for (i = 0; i < n; i++) { // eslint-disable-line no-plusplus
+    x += y;
+    y = x - y;
+  }
+  return y;
+}
+
+fib(3);
